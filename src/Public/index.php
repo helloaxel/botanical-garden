603e9a74404578585bba4566e99d8b21c073fae6
helloaxel <160547598+helloaxel@users.noreply.github.com>
2026-02-18 22:58:54 +0100

    Remove duplicate src directory

--- src/Public/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../Controller/PlantaController.php';
-
-$controlador = new PlantaController();
-$controlador->mostrarLista();
-
-
-?>
